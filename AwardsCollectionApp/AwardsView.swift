2de710d0b744a41cf79749c42f53960bcaad5fa6
Alik <Nigay_AO@bk.ru>
2022-02-10 12:56:11 +0300

    Initial Commit

---
+++ AwardsCollectionApp/AwardsView.swift
@@ -0,0 +1,36 @@
+//
+//  AwardsView.swift
+//  AwardsCollectionApp
+//
+//  Created by Alexey Efimov on 17.06.2021.
+//
+
+import SwiftUI
+
+struct AwardsView: View {
+    let awards = Award.getAwards()
+    let columns = [GridItem(.adaptive(minimum: 160, maximum: 200))]
+    var activeAwards: [Award] {
+        awards.filter { $0.awarded }
+    }
+    
+    var body: some View {
+        NavigationView {
+            CustomGridView(items: activeAwards, columns: 2) { sizeItem, award in
+                VStack {
+                    award.awardView
+                    Text(award.title)
+                }
+                .padding()
+                .frame(width: sizeItem, height: sizeItem)
+            }
+            .navigationBarTitle("Your awards: \(activeAwards.count)")
+        }
+    }
+}
+
+struct AwardsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AwardsView()
+    }
+}
